afad5518f95949a2390765b080c63494b9050a5d
Danielov1 <63906099+danielov1@users.noreply.github.com>
2021-04-28 14:05:24 +0300

    Add files via upload

---
+++ excel.py
@@ -0,0 +1,177 @@
+#! /usr/bin/env python
+
+import pandas as pd
+import numpy as np
+import datetime
+import calendar
+import itertools
+import xlsxwriter
+import openpyxl
+from openpyxl import Workbook
+import json
+import sys
+import requests
+import math
+import numpy
+
+## List NOC users from opsgenie
+noc_manager = <name>
+genie_token = <api-teken>
+
+url = "https://api.eu.opsgenie.com/v2/users/?query=skypeUsername=NOC"
+
+payload={}
+headers = {
+  'Authorization': 'GenieKey '+ genie_token,
+}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+
+response_Org = response.text
+sys.stdout.flush()
+response_json = json.loads(response_Org)
+total_count = int(response_json['totalCount'])
+
+
+## Check what is the current date
+
+now = datetime.datetime.now()
+current_year = now.year
+current_month = now.month
+next_month = current_month + 1
+
+## Check what is the next month
+
+month_num_str = str(next_month)
+datetime_object = datetime.datetime.strptime(month_num_str, "%m")
+full_month_name = datetime_object.strftime("%B")
+
+## Create file named by the next month name
+file_loc = '~/Desktop/opsgenie/'+full_month_name+'.xlsx'
+
+
+num_days_in_month = calendar.monthrange(current_year, current_month)[1]
+next_date = f"'{current_year}-{next_month}-01"
+rng = pd.date_range(next_date, periods=num_days_in_month, freq='D')
+
+## Functions for date listing
+
+def take(n, iterable):
+    "Return first n items of the iterable as a list"
+    return list(itertools.islice(iterable, n))
+
+def pairwise(iterable):
+    "s -> (s0,s1), (s1,s2), (s2, s3), ..."
+    a, b = itertools.tee(iterable)
+    next(b, None)
+    return zip(a, b)
+
+def single(iterable): 
+    "s -> (s0,s1), (s1,s2), (s2, s3), ..."
+    a = iterable
+    return a
+
+## Create date frame for the next month
+
+day = ''
+name = ''
+user = ''
+rotation_id = ''
+times = itertools.cycle( ['01:00:00', '09:00:00', '17:00:00'])
+rotations = itertools.cycle( ['Night', 'Day', 'Evening'])
+empty_one = ''
+employees_list = []
+fullname_list = []
+columns_array = ['Day','StartDate', 'EndDate', 'StartTime', 'EndTime', 'RotationName', 'RotationID', 'Name', 'User','']
+
+for arr_index in columns_array:
+    employees_list.append(arr_index)
+
+x = 0
+for x in range(total_count):
+    fullname_list.append(response_json['data'][x]['fullName'])
+
+
+dates = list(itertools.chain.from_iterable(itertools.repeat(timestamp.date(), 3) for timestamp in rng))
+
+paired_dates = pairwise(take(num_days_in_month*3, dates))
+paired_times = pairwise(take(num_days_in_month*3, times))
+paired_rotations = single(take(num_days_in_month*3, rotations))
+
+
+df = pd.DataFrame([[day, start_date, end_date, start_time, end_time, rotation_name, rotation_id, name, user, empty_one] for (start_date, end_date), (start_time, end_time), (rotation_name) in zip(paired_dates, paired_times, paired_rotations)],
+               columns =columns_array)
+
+## Add new columns with emplyees names
+
+x = 0
+for x in range(total_count):
+    df = df.assign(**{fullname_list[x]: ""})
+
+y = 0
+for y in range(num_days_in_month*3):
+    if y%3==1:
+        df[noc_manager][y] = 'V'
+        df['Name'][y] = noc_manager
+    elif y%3!=1:
+        df[noc_manager][y] = 'X'
+
+
+## format Date cells to Short Date
+
+df['StartDate'] = pd.to_datetime(df['StartDate'], errors='coerce').dt.strftime('%d/%m/%Y')
+df['EndDate'] = pd.to_datetime(df['EndDate'], errors='coerce').dt.strftime('%d/%m/%Y')
+
+
+## Add rotaion id formula
+num_for_rotation = num_days_in_month * 3
+
+i = 0
+while i < num_for_rotation:
+    i_str = i + 2
+    i_str = str(i_str)
+    df['Day'][i] = '=TEXT(B'+i_str+',"dddd")'
+    df['RotationID'][i] = '=IF(F'+i_str+'="Evening","38fa4389-904e-442a-87d7-36dc0dc554b9",IF(F'+i_str+'="Night","3e558a75-9e32-48a2-8123-46f0aa448154",IF(F'+i_str+'="Day","81510d4f-fc2e-4c08-a436-35bbbb6b5384")))'
+
+    ## List of Employees
+    
+
+    username_list = []
+    first_name = []
+    formula_var = ''
+    split_fullname = ''
+
+    formula_temp = ''
+    max_if = 0
+    loopif = math.ceil(total_count/7)
+    innerloopcount = 7
+    x = 0
+    empty = '""'
+
+    for z in range(loopif): 
+        if loopif-z == 1:
+            innerloopcount = total_count%7
+        formula_temp = ''
+        for in_loop in range(innerloopcount):
+            x = in_loop + (z*7)
+
+            username_list.append(response_json['data'][x]['username'])
+            fullname_list.append(response_json['data'][x]['fullName'])
+            
+            formula_temp = 'IF(H'+i_str+'=''"'+fullname_list[x]+'"'',''"'+username_list[x]+'"'','+ formula_temp +')'
+
+            formula_temp=formula_temp.replace(",)",",\"\")")
+            if(z != 0 and z%7== 0):
+                formula_temp = '=' + formula_temp
+                break
+        formula_var = formula_temp + " & " + formula_var
+        formula_var = formula_var[:-1]
+    formula_var = '=' + formula_var
+    formula_var = formula_var[:-1]
+    df['User'][i] = formula_var
+    
+    i = i + 1
+
+## Write to Excel file
+
+df.to_excel(file_loc, index = False)
---
+++ main.py
@@ -0,0 +1,130 @@
+#! /usr/bin/env python
+
+import pandas as pd
+import json
+import requests
+import sys
+from datetime import datetime
+from pytz import timezone
+import datetime
+import pytz
+
+loc = '<Excel-Path>'
+schedule_id = '<Schedule-id>'
+genie_token = '<Token>'
+
+def prefix_time(x):
+    x = list(x)
+    prefix_zero = x[0]
+    prefix_zero = str(prefix_zero)
+    if prefix_zero == '0':
+        prefix_zero = x[1]
+        return prefix_zero
+    else:
+        x = ''.join(x)
+        return x
+
+
+## Function that convert time from Asia/Jerusalem to UTC
+
+def change_time(m_date, m_time):
+    time_zone = timezone('Asia/Jerusalem')
+    m_date = m_date.replace('/','-')
+    my_time = m_date + " " + m_time
+
+    changed_time = time_zone.localize(datetime.datetime.strptime(my_time, '%d-%m-%Y %H:%M:%S')).astimezone(pytz.utc)
+
+    changed_time = str(changed_time)
+    changed_time = changed_time[:-6]
+    split_date = changed_time.split()
+    time_split = split_date[1]
+    date_split = split_date[0]
+    return time_split, date_split
+
+## Function that sort Dataframe by Rotation Name and execute PATCH API call that update the schedule
+
+def sort_df(rotation_name, rotation_id):
+
+    user_list = []
+    start_date_list = []
+    start_time_list = []
+    end_date_list = []
+    end_time_list = []
+
+    df = pd.read_excel(loc)
+    rslt_df = df[df["RotationName"] == rotation_name]
+    df = rslt_df.groupby(['StartDate', 'StartTime', 'EndDate', 'EndTime', 'User']).agg(tuple).reset_index()
+    
+    for record in df.to_dict(orient='records'):
+        start_date_list.append(str(record['StartDate']).split()[0])
+        start_time_list.append(str(record['StartTime']))
+        end_date_list.append(str(record['EndDate']).split()[0])
+        end_time_list.append(str(record['EndTime']))
+        user_list.append(str(record['User']))
+    
+
+    ## Get the first Start Date and the first Start Time
+    #print(start_date_list)
+    start_date = start_date_list[0]
+    start_time = start_time_list[0]
+    prefix_start_time_h = start_time.split(":")[0]
+    prefix_start_time_m = start_time.split(":")[1] 
+    prefix_start_time_h = prefix_time(prefix_start_time_h)
+    prefix_start_time_m = prefix_time(prefix_start_time_m)
+
+    ## Get the last End Date and the last End Start Time
+
+    end_date = end_date_list[-1]
+    end_time = end_time_list[-1]
+    prefix_end_time_h = end_time.split(":")[0]
+    prefix_end_time_m = end_time.split(":")[1] 
+    prefix_end_time_h = prefix_time(prefix_end_time_h)
+    prefix_end_time_m = prefix_time(prefix_end_time_m)
+
+    ## Convert time and date to UTC
+
+    time_split, date_split = change_time(start_date, start_time)
+    my_start_date_split = date_split
+    my_start_time_split = time_split
+
+    time_split, date_split = change_time(end_date, end_time)
+    my_end_date_split = date_split
+    my_end_time_split = time_split
+    
+
+    ## Get User List to JSON
+    
+    user_list_json = {"participants": [{ "type": "user", "username": i} for i in user_list]}
+    json.dumps(user_list_json)
+    user_list_json = str(user_list_json)
+    user_list_json = user_list_json[1:-1]
+    user_list_json = user_list_json.replace("'",'"')
+
+
+    ## PATCH API call that update the schedule
+    
+
+    headers = {
+        'Authorization': 'GenieKey '+ genie_token,
+        'Content-Type': 'application/json',
+    }
+
+    data = ' { "name": "'+ rotation_name +'", "startDate": "'+ my_start_date_split +'T'+ my_start_time_split + 'Z", "endDate": "'+ my_end_date_split +'T'+ my_end_time_split + 'Z", "type": "daily", "length": 1, '+ user_list_json + ', "timeRestriction": { "type": "time-of-day", "restriction": { "startHour": '+ prefix_start_time_h +', "endHour": '+ prefix_end_time_h +', "startMin": '+ prefix_start_time_m +', "endMin": '+ prefix_end_time_m +' } } }'
+    response = requests.patch('https://api.eu.opsgenie.com/v2/schedules/'+ schedule_id +'/rotations/'+ rotation_id, headers=headers, data=data)
+
+    response_Org = response.text
+    sys.stdout.flush()
+    response_json = json.loads(response_Org)
+    print(response_json)
+
+## Read excel file to Dataframe
+
+df = pd.read_excel(loc)
+df = df.groupby(['RotationName', 'RotationID']).agg(tuple).reset_index()
+
+## Run function for each Rotation name
+
+for record in df.to_dict(orient='records'):
+    rotation_name = (str(record['RotationName']))
+    rotation_id = (str(record['RotationID']))
+    sort_df(rotation_name, rotation_id)
